a7bab3cf442bb153c357c01fd4841ea266081d7e
princerafid01 <mahmudrafid02@gmail.com>
2025-09-28 06:16:02 +0600

    feat: implementing config through ENV

---
+++ config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Version     string
+	ServiceName string
+	HttpPort    int64
+}
+
+var configurations Config
+
+func loadConfig() {
+	version := os.Getenv("VERSION")
+	if version == "" {
+		fmt.Println("Version is required")
+		os.Exit(1)
+	}
+
+	serviceName := os.Getenv("SERVICE_NAME")
+	if serviceName == "" {
+		fmt.Println("Service Name is required")
+		os.Exit(1)
+	}
+
+	httpPort := os.Getenv("HTTP_PORT")
+	if httpPort == "" {
+		fmt.Println("Http Port is required")
+		os.Exit(1)
+	}
+
+	port, err := strconv.ParseInt(httpPort, 10, 64)
+
+	if err != nil {
+		fmt.Println("Port Must be a number")
+	}
+
+	configurations = Config{
+		Version:     version,
+		ServiceName: serviceName,
+		HttpPort:    port,
+	}
+
+}
+
+func GetConfig() Config {
+	loadConfig()
+	return configurations
+}
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"ecommerce/cmd"
+	"ecommerce/config"
 )
 
 func main() {
+	cnf := config.GetConfig()
+
 	cmd.Serve()
 }
